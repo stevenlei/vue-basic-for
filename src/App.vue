9ea9396e6b379a3cc56991c72725c3eb1dea58e0
Steven Lei <stevenplus@gmail.com>
2022-08-12 00:39:32 +0800

    init vite + vue + tailwind and basic ui

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      words: "",
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="min-h-screen w-full bg-slate-100 p-12">
+    <div class="bg-white flex-1 rounded-xl shadow-xl overflow-hidden">
+      <input class="text-5xl font-semibold text-center text-gray-800 py-12 w-full outline-none" v-model="words" />
+    </div>
+    <ul class="flex flex-wrap gap-12 mt-16">
+      <li class="inline-block px-12 py-8 rounded-xl shadow-lg bg-blue-600 text-white text-5xl">Apple</li>
+    </ul>
+  </div>
+</template>
